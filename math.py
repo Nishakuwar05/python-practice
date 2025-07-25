aee9fcf41230dcb6225c5fd9e46b12d3a5e0f868
Nishakuwar05 <nishakuwar05@gmail.com>
2025-07-25 20:11:49 +0530

    Create math.py
    maths basic done

---
+++ math.py
@@ -0,0 +1,14 @@
+import math
+x=min(5,10,25)
+y=max(5,10,25)
+z=abs(-10)
+a=pow(2,3)
+print("Minimum value:", x)#returns the minimum value from the given numbers
+print("Maximum value:", y)#returns the maximum value from the given numbers
+
+print("Absolute value:", z)#returns the absolute value of a number
+
+print("Power value (2^3):", a)#returns 2 raised to the power of 3
+print("Square root of 16:", math.sqrt(16))#returns the square root of a number
+print("the ceil value of 1.4 is:", math.ceil(1.4))#returns the smallest integer greater than or equal to the given number
+print ("the floor value of 1.4 is:", math.floor(1.4))#returns the largest integer less than or equal to the given number
