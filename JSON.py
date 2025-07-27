92f51f81d88df846e00089b23892a0c0a58639f6
Nishakuwar05 <nishakuwar05@gmail.com>
2025-07-27 17:35:42 +0530

    updated json

--- JSON.py
+++ JSON.py
@@ -6,5 +6,8 @@ student = {
     "subjects": ["Math", "Science", "English"],
     "grades": { "algebra": 90, "biology": 85, "history": 88 }
 }
-y=json.dumps(student)
-print(y)
+y=json.dumps(student,indent=4,sort_keys=True)#convert python object to json string
+print(y) #indent is used to make the json string more readable
+z='{"name": "Nisha","age": 20, "subjects": ["Math", "Science", "English"], "grades": {"algebra": 90, "biology": 85, "history": 88}}'
+x=json.loads(z)#covert json string to python object
+print(x)
